551f18317b18836a7c9e7abea847d2ba7e190815
Mert Adalı <mertadali605@gmail.com>
2024-07-10 17:22:59 +0300

    PeriodicWorkRequest used!

--- app/src/main/java/com/mertadali/kotlinworkmanager/MainActivity.kt
+++ app/src/main/java/com/mertadali/kotlinworkmanager/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mertadali.kotlinworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.work.*
 import com.mertadali.kotlinworkmanager.databinding.ActivityMainBinding
 import java.util.concurrent.TimeUnit
@@ -22,15 +23,57 @@ class MainActivity : AppCompatActivity() {
             .setRequiresCharging(false)
             .build()
 
+        /* Bir kereye mahsus olan işler için alttaki kısım kullanılacak.
+
         val myWorkRequest : WorkRequest = OneTimeWorkRequestBuilder<RefreshDatabase>()
             .setConstraints(constraints)
             .setInputData(data)
-           // .setInitialDelay(5,TimeUnit.MINUTES)
+            .setInitialDelay(5,TimeUnit.MINUTES)
             .addTag("firstWork")
             .build()
 
-
         WorkManager.getInstance(this).enqueue(myWorkRequest)
+        */
+
+        // Periodic işlemler için kullanılacak yapı.
+
+        val myPerWorkRequest : PeriodicWorkRequest = PeriodicWorkRequestBuilder<RefreshDatabase>(15,TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .setInputData(data)
+            .addTag("secondWork")
+            .build()
+
+        WorkManager.getInstance(this).enqueue(myPerWorkRequest)
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(myPerWorkRequest.id).observe(this,
+            Observer {
+                if (it.state == WorkInfo.State.RUNNING){
+                    println("running")
+
+                }else if (it.state == WorkInfo.State.FAILED){
+                    println("failed")
+
+                }else if (it.state == WorkInfo.State.SUCCEEDED){
+                    println("succeeded")
+                }
+            })
+
+        // İşleri iptal etmek için:
+        //WorkManager.getInstance(this).cancelAllWork()
+
+        // Chaining - Zincirleme yani bu iş bittikten sonra bunu yap gibi işlemler için kullanılır. Bu işlem sadece OneTimeRequestlerde kullanılır.
+
+        val oneTimeWorkRequest : OneTimeWorkRequest = OneTimeWorkRequestBuilder<RefreshDatabase>()
+            .setConstraints(constraints)
+            .setInputData(data)
+            .addTag("chainingWork")
+            .build()
+
+        WorkManager.getInstance(this)
+            .beginWith(oneTimeWorkRequest)
+            .then(oneTimeWorkRequest)
+            .enqueue()
+
 
 
 
